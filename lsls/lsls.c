655184ecfd7b61a1467e63931a238a615819f6f9
theryanclausen <theryanclausen@gmail.com>
2019-03-22 09:22:05 -0700

    print file names from parsed command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,18 +6,26 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *sd;
+
   // Parse command line
-  char *input = "howdy";
+  char *input = ".";
   if (argc > 1)
   {
     input = argv[1];
   }
-  printf("%s\n", input);
+
   // Open directory
+  dir = opendir(input);
 
   // Repeatly read and print entries
+  while((sd = readdir(dir)) != NULL)
+  {
+    printf("%s\n", sd->d_name);
+  }
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
